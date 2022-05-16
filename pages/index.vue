d02cd2bf03a20849e0ff56ea5dc6a2ae487d0f48
hendras1722 <syahendra@privyid.tech>
2022-05-16 18:01:54 +0700

    check version

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,23 @@
       Jaga rahasia diantara kita jangan sampai disebar kemana mana ini otp buat
       kamu ya: 12345
     </div>
+    {{ heh }}
     <div>@webgua.com #12345</div>
     <br />
     <div>Otpmu nii buat dikirim: {{ stateOTP }}</div>
     <div>sayangnya belum ada input otpnya ini cuman ngetest doang</div>
+    <div>v.{{ version }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "IndexPage",
-  data: {
-    stateOTP: "",
+  data() {
+    return {
+      stateOTP: "",
+      version: "1.0.0",
+    };
   },
   mounted() {
     if ("OTPCredential" in window) {
